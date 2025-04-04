0d92d019296eb691aa5ead2c5a0c7c939ac02db5
eli-wint <e.winton.cc@gmail.com>
2025-04-03 20:42:32 -0700

    Finished Calculator

--- Calculator.py
+++ Calculator.py
@@ -5,25 +5,31 @@ root = tk.Tk()
 displayNumber = 0
 currentValue = 0
 result = 0
+pastResult = 0
 
-numericalEntry = tk.Entry(root)
-equalsButton = tk.Button(root, text = "=")
-displayAnswer = tk.Label(root, text = f"{displayNumber}")
-additionButton = tk.Button(root, text = "+")
-oneButton = tk.Button(root, text = "1")
-twoButton = tk.Button(root, text = "2")
-threeButton = tk.Button(root, text = "3")
-fourButton = tk.Button(root, text = "4")
-fiveButton = tk.Button(root, text = "5")
-sixButton = tk.Button(root, text = "6")
-sevenButton = tk.Button(root, text = "7")
-eightButton = tk.Button(root, text = "8")
-nineButton = tk.Button(root, text = "9")
-zeroButton = tk.Button(root, text = "0")
+numericalEntry = tk.Entry(root, justify = 'center')
+numericalEntry.pack()
+equalsButton = tk.Button(root, text = "=", width = 17)
+displayAnswer = tk.Label(root, text = f"{displayNumber}", font = ("HELVETICA", 20))
+additionButton = tk.Button(root, text = "+", width = 17)
+subtractionButton = tk.Button(root, text = "-", width = 17)
+multiplicationButton = tk.Button(root, text = "x", width = 17)
+divisionButton = tk.Button(root, text = "%", width = 17)
+clearButton = tk.Button(root, text = "CLEAR ALL")
+oneButton = tk.Button(root, text = "1", width = 17)
+twoButton = tk.Button(root, text = "2", width = 17)
+threeButton = tk.Button(root, text = "3", width = 17)
+fourButton = tk.Button(root, text = "4", width = 17)
+fiveButton = tk.Button(root, text = "5", width = 17)
+sixButton = tk.Button(root, text = "6", width = 17)
+sevenButton = tk.Button(root, text = "7", width = 17)
+eightButton = tk.Button(root, text = "8", width = 17)
+nineButton = tk.Button(root, text = "9", width = 17)
+zeroButton = tk.Button(root, text = "0", width = 17)
 
 # Putting variables into the grid
-numericalEntry.grid(column = 0, row = 0)
-displayAnswer.grid(column = 0, row = 1)
+numericalEntry.grid(column = 1, row = 0)
+displayAnswer.grid(column = 1, row = 1)
 oneButton.grid(column = 0, row = 2, sticky="w")
 twoButton.grid(column = 1, row = 2, sticky="w")
 threeButton.grid(column = 2, row = 2, sticky="w")
@@ -35,12 +41,19 @@ eightButton.grid(column = 1, row = 4, sticky="w")
 nineButton.grid(column = 2, row = 4, sticky="w")
 additionButton.grid(column = 0, row = 5, sticky="w")
 zeroButton.grid(column = 1, row = 5, sticky="w")
-equalsButton.grid(column = 2, row = 5, sticky="w")
+multiplicationButton.grid(column = 2, row = 5, sticky="w")
+subtractionButton.grid(column = 0, row = 6, sticky = "w")
+equalsButton.grid(column = 1, row = 6, sticky = "w")
+divisionButton.grid(column = 2, row = 6, sticky = "w")
+clearButton.grid(column = 1, row = 7)
 
 def CalculateEntry():
-    global displayNumber, currentValue, result
+    global displayNumber, currentValue, result, pastResult
 
-    currentValue = numericalEntry.get()
+    if (pastResult != 0):
+        currentValue = (str(pastResult) + numericalEntry.get())
+    else:
+        currentValue = numericalEntry.get()
 
     numericalEntry.delete(0, tk.END)
 
@@ -48,13 +61,34 @@ def CalculateEntry():
 
     displayAnswer.config(text = f"{result}")
 
-def InsertButtonInput(selectedButton):
-    global numericalEntry
-    # (variable).insert(num, text)
+    pastResult = result
 
+def InsertButtonInput(selectedButton):
+    global numericalEntry, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton, zeroButton, additionButton
+    # (variable).insert(num, text)
+    numericalEntry.insert(tk.END, selectedButton)
+
+def ClearAllCommand():
+    global numericalEntry, displayAnswer, pastResult
+    numericalEntry.delete(0, tk.END)
+    displayAnswer.config(text = "0")
+    pastResult = 0
 
 equalsButton.config(command= lambda: CalculateEntry())
-additionButton.config()
-
+oneButton.config(command=lambda: InsertButtonInput("1"))
+twoButton.config(command=lambda: InsertButtonInput("2"))
+threeButton.config(command=lambda: InsertButtonInput("3"))
+fourButton.config(command=lambda: InsertButtonInput("4"))
+fiveButton.config(command=lambda: InsertButtonInput("5"))
+sixButton.config(command=lambda: InsertButtonInput("6"))
+sevenButton.config(command=lambda: InsertButtonInput("7"))
+eightButton.config(command=lambda: InsertButtonInput("8"))
+nineButton.config(command=lambda: InsertButtonInput("9"))
+zeroButton.config(command=lambda: InsertButtonInput("0"))
+additionButton.config(command= lambda: InsertButtonInput("+"))
+subtractionButton.config(command= lambda: InsertButtonInput("-"))
+multiplicationButton.config(command= lambda: InsertButtonInput("*"))
+divisionButton.config(command= lambda: InsertButtonInput("/"))
+clearButton.config(command= lambda: ClearAllCommand())
 
 root.mainloop()
